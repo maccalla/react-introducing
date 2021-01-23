d093a4233c7a35e5483b6fd8913ca8f42e6ad538
maccalla <bcmk1220158@gn.iwasaki.ac.jp>
2021-01-23 22:49:21 +0900

    [update]JSXの導入

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
 
 export default function App() {
+  const name = "john ashe";
+  const user = {
+    firstName: "kanna",
+    lastName: "shinme"
+  };
+
+  function formatName(user) {
+    return user.firstName + " " + user.lastName;
+  }
+  function renderIfCompornent(bool) {
+    if (bool) {
+      return <div>hello true world!!</div>;
+    } else {
+      return <div>hello false world!!</div>;
+    }
+  }
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <h2>{name}</h2>
+      <p>{formatName(user)}</p>
       <h2>Start editing to see some magic happen!</h2>
+      <div>{renderIfCompornent(true)}</div>
     </div>
   );
 }
