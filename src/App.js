ddab9b46006ca3f57e2c9120a52c8df998238ab3
maccalla <bcmk1220158@gn.iwasaki.ac.jp>
2021-02-14 00:38:46 +0900

    [change]コンポーネントとprops

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ export default function App() {
   function formatName(user) {
     return user.firstName + " " + user.lastName;
   }
+  function formatDate(date) {
+    return date.toLocaleDateString();
+  }
   function renderIfCompornent(bool) {
     if (bool) {
       return <div>hello true world!!</div>;
@@ -17,6 +20,44 @@ export default function App() {
       return <div>hello false world!!</div>;
     }
   }
+  //関数コンポーネント
+  function WelcomeFunc(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+
+  function Avatar(props) {
+    return (
+      <img className="Avatar" src={props.usr.avatarUrl} alt={props.user.name} />
+    );
+  }
+
+  function UserInfo(props) {
+    return (
+      <div className="UserInfo">
+        <Avatar user={props.user} />
+        <div className="UserInfo-name">{props.user.name}</div>
+      </div>
+    );
+  }
+
+  function Comment(props) {
+    return (
+      <div className="Comment">
+        <UserInfo user={props.author} />
+        <div className="Comment-text">{props.text}</div>
+        <div className="Comment-date">{formatDate(props.date)}</div>
+      </div>
+    );
+  }
+
+  //propsは読み取り専用
+  function sum(a, b) {
+    return a + b; //純粋(pure)→純関数
+  }
+  function withdraw(account, amount) {
+    account.total -= amount; //自身への入力を変更するため純関数ではない
+  }
+  //すべてのReactコンポーネントは自己のpropsに対して純関数のように振るわねばならない
 
   return (
     <div className="App">
@@ -25,6 +66,10 @@ export default function App() {
       <p>{formatName(user)}</p>
       <h2>Start editing to see some magic happen!</h2>
       <div>{renderIfCompornent(true)}</div>
+      {/* 関数コンポーネントの組み込み */}
+      <WelcomeFunc name="manG" />
+      <WelcomeFunc name="manH" />
+      {/* コンポーネントの抽出（分割） */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import React from "react";
 
 import App from "./App";
 
@@ -22,3 +23,23 @@ function tick() {
 }
 
 setInterval(tick, 1000);
+
+//下記2つは等価
+//関数コンポーネント
+// function WelcomeFunc(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+//クラスコンポーネント
+class WelcomeClass extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+//JSXにかかれている属性と子要素を単一のオブジェクトとしてこのコンポーネントに渡す
+function renderComponent() {
+  const element = <WelcomeClass name="Sara" />;
+  ReactDOM.render(element, document.getElementById("component"));
+}
+
+renderComponent();
